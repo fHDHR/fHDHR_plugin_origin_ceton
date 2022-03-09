7088b4ccb713487a556687d9142d940c1b380f1d
deathbybandaid <github@deathbybandaid.net>
2022-03-08 19:48:15 -0500

    Upstream Changes from core, minor linting and tweaking

--- web/ceton_html.py
+++ web/ceton_html.py
@@ -14,13 +14,14 @@ class Ceton_HTML():
         self.fhdhr = fhdhr
         self.plugin_utils = plugin_utils
 
-        self.origin = plugin_utils.origin
+        self.origin_obj = plugin_utils.origin_obj
+        self.origin_name = self.origin_obj.name
 
         self.template_file = pathlib.Path(plugin_utils.path).joinpath('ceton.html')
         self.template = StringIO()
         self.template.write(open(self.template_file).read())
 
-        self.hwtype = self.plugin_utils.origin.get_ceton_getvar(0, "HostConnection")
+        self.hwtype = self.plugin_utils.origin_obj.get_ceton_getvar(0, "HostConnection")
         if 'pci' in self.hwtype:
             self.ceton_pcie = True
         else:
@@ -48,24 +49,24 @@ class Ceton_HTML():
 
         if self.origin.setup_success:
             origin_status_dict = {"Setup": "Success"}
-            origin_status_dict["Temp"] = self.plugin_utils.origin.get_ceton_getvar(0, "Temperature")
+            origin_status_dict["Temp"] = self.plugin_utils.origin_obj.get_ceton_getvar(0, "Temperature")
             origin_status_dict["HWType"] = self.hwtype
-            origin_status_dict["HostHardware"] = self.plugin_utils.origin.get_ceton_getvar(0, "HostHardware")
-            origin_status_dict["HostFirmware"] = self.plugin_utils.origin.get_ceton_getvar(0, "HostFirmware")
-            origin_status_dict["HostSerial"] = self.plugin_utils.origin.get_ceton_getvar(0, "HostSerial")
+            origin_status_dict["HostHardware"] = self.plugin_utils.origin_obj.get_ceton_getvar(0, "HostHardware")
+            origin_status_dict["HostFirmware"] = self.plugin_utils.origin_obj.get_ceton_getvar(0, "HostFirmware")
+            origin_status_dict["HostSerial"] = self.plugin_utils.origin_obj.get_ceton_getvar(0, "HostSerial")
 
             for i in range(int(self.fhdhr.config.dict["ceton"]["tuners"])):
                 origin_status_dict["Tuner"+str(i)] = {}
-                origin_status_dict["Tuner" + str(i)]['Transport'] = self.plugin_utils.origin.get_ceton_getvar(i, "TransportState")
+                origin_status_dict["Tuner" + str(i)]['Transport'] = self.plugin_utils.origin_obj.get_ceton_getvar(i, "TransportState")
                 origin_status_dict["Tuner"+str(i)]['HWState'] = self.devinuse("/dev/ceton/ctn91xx_mpeg0_%s" % i)
-                origin_status_dict["Tuner"+str(i)]['Channel'] = self.plugin_utils.origin.get_ceton_getvar(i, "Signal_Channel")
-                origin_status_dict["Tuner"+str(i)]['SignalLock'] = self.plugin_utils.origin.get_ceton_getvar(i, "SignalCarrierLock")
-                origin_status_dict["Tuner"+str(i)]['PCRLock'] = self.plugin_utils.origin.get_ceton_getvar(i, "SignalPCRLock")
-                origin_status_dict["Tuner"+str(i)]['Signal'] = self.plugin_utils.origin.get_ceton_getvar(i, "Signal_Level")
-                origin_status_dict["Tuner"+str(i)]['SNR'] = self.plugin_utils.origin.get_ceton_getvar(i, "Signal_SNR")
-                origin_status_dict["Tuner"+str(i)]['BER'] = self.plugin_utils.origin.get_ceton_getvar(i, "Signal_BER")
-                origin_status_dict["Tuner"+str(i)]['Modulation'] = self.plugin_utils.origin.get_ceton_getvar(i, "Signal_Modulation")
+                origin_status_dict["Tuner"+str(i)]['Channel'] = self.plugin_utils.origin_obj.get_ceton_getvar(i, "Signal_Channel")
+                origin_status_dict["Tuner"+str(i)]['SignalLock'] = self.plugin_utils.origin_obj.get_ceton_getvar(i, "SignalCarrierLock")
+                origin_status_dict["Tuner"+str(i)]['PCRLock'] = self.plugin_utils.origin_obj.get_ceton_getvar(i, "SignalPCRLock")
+                origin_status_dict["Tuner"+str(i)]['Signal'] = self.plugin_utils.origin_obj.get_ceton_getvar(i, "Signal_Level")
+                origin_status_dict["Tuner"+str(i)]['SNR'] = self.plugin_utils.origin_obj.get_ceton_getvar(i, "Signal_SNR")
+                origin_status_dict["Tuner"+str(i)]['BER'] = self.plugin_utils.origin_obj.get_ceton_getvar(i, "Signal_BER")
+                origin_status_dict["Tuner"+str(i)]['Modulation'] = self.plugin_utils.origin_obj.get_ceton_getvar(i, "Signal_Modulation")
         else:
             origin_status_dict = {"Setup": "Failed"}
 
-        return render_template_string(self.template.getvalue(), request=request, fhdhr=self.fhdhr, origin_status_dict=origin_status_dict, list=list)
+        return render_template_string(self.template.getvalue(), request=request, fhdhr=self.fhdhr, origin_name=self.origin_name, origin_status_dict=origin_status_dict, list=list)
